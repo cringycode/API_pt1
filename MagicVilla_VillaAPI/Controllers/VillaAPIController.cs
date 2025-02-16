d4d32df36e23a4bfdeefd6cd2d520fc919bfd368
kourosh karimi <karimikorosh2000@gmail.com>
2025-02-16 20:36:04 +0330

    update action

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -56,7 +56,7 @@ public class VillaAPIController : ControllerBase
         // {
         //     return BadRequest(ModelState);
         // }
-        
+
         if (villaDTO is null)
         {
             return BadRequest();
@@ -73,6 +73,53 @@ public class VillaAPIController : ControllerBase
 
         return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
     }
- 
+
+    #endregion
+
+    #region DELETE
+
+    [HttpDelete("{id:int}", Name = "DeleteVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult DeleteVilla(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        if (villa is null)
+        {
+            return NotFound();
+        }
+
+        VillaStore.villaList.Remove(villa);
+        return NoContent();
+    }
+
+    #endregion
+
+    #region UPDATE
+
+    [HttpPut("{id:int}", Name = "UpdateVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+    {
+        if (villaDTO == null || id != villaDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        villa.Name = villaDTO.Name;
+        villa.Sqft = villaDTO.Sqft;
+        villa.Occupancy = villaDTO.Occupancy;
+
+        return NoContent();
+    }
+
     #endregion
 }
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,7 +6,7 @@ public static class VillaStore
 {
     public static List<VillaDTO> villaList = new List<VillaDTO>()
     {
-        new VillaDTO { Id = 1, Name = "Pool View" },
-        new VillaDTO { Id = 2, Name = "Beach view" }
+        new VillaDTO { Id = 1, Name = "Pool View", Sqft = 100, Occupancy = 4 },
+        new VillaDTO { Id = 2, Name = "Beach view", Sqft = 100, Occupancy = 3 }
     };
 }
