e6fbfb73a2d8a4f5b65c1899156c76bf731fc8f7
kourosh karimi <karimikorosh2000@gmail.com>
2025-02-19 19:35:57 +0330

    Seed Villa table

--- MagicVilla_VillaAPI/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MagicVilla_VillaAPI/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -64,6 +64,73 @@ namespace MagicVilla_VillaAPI.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Villas");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Amenity = "",
+                            CreatedDate = new DateTime(2025, 2, 19, 19, 32, 16, 117, DateTimeKind.Local).AddTicks(4904),
+                            Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                            ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg",
+                            Name = "Royal Villa",
+                            Occupancy = 4,
+                            Rate = 200.0,
+                            Sqft = 550,
+                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Amenity = "",
+                            CreatedDate = new DateTime(2025, 2, 19, 19, 32, 16, 117, DateTimeKind.Local).AddTicks(4916),
+                            Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                            ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg",
+                            Name = "Premium Pool Villa",
+                            Occupancy = 4,
+                            Rate = 300.0,
+                            Sqft = 550,
+                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Amenity = "",
+                            CreatedDate = new DateTime(2025, 2, 19, 19, 32, 16, 117, DateTimeKind.Local).AddTicks(4918),
+                            Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                            ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg",
+                            Name = "Luxury Pool Villa",
+                            Occupancy = 4,
+                            Rate = 400.0,
+                            Sqft = 750,
+                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Amenity = "",
+                            CreatedDate = new DateTime(2025, 2, 19, 19, 32, 16, 117, DateTimeKind.Local).AddTicks(4919),
+                            Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                            ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg",
+                            Name = "Diamond Villa",
+                            Occupancy = 4,
+                            Rate = 550.0,
+                            Sqft = 900,
+                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Amenity = "",
+                            CreatedDate = new DateTime(2025, 2, 19, 19, 32, 16, 117, DateTimeKind.Local).AddTicks(4921),
+                            Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                            ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg",
+                            Name = "Diamond Pool Villa",
+                            Occupancy = 4,
+                            Rate = 600.0,
+                            Sqft = 1100,
+                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+                        });
                 });
 #pragma warning restore 612, 618
         }
