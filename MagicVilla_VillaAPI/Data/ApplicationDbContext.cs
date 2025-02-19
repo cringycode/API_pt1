ce47d6aee3a370fc0512fcc0249393313c5bb91d
kourosh karimi <karimikorosh2000@gmail.com>
2025-02-19 19:15:07 +0330

    Villa Table

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+          
     }
     
     public DbSet<Villa> Villas { get; set; }
