fad686ef8f5536f5688982bfadbb0f9130bdfd2c
kourosh karimi <karimikorosh2000@gmail.com>
2025-02-19 21:15:19 +0330

    Villa update & create DTO.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers;
 
@@ -62,7 +63,7 @@ public class VillaAPIController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+    public ActionResult<VillaDTO> CreateVilla([FromBody] VillaCreateDTO villaDTO)
     {
         // if (!ModelState.IsValid)
         // {
@@ -74,16 +75,15 @@ public class VillaAPIController : ControllerBase
             return BadRequest();
         }
 
-        if (villaDTO.Id > 0)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        }
+        // if (villaDTO.Id > 0)
+        // {
+        //     return StatusCode(StatusCodes.Status500InternalServerError);
+        // }
 
         Villa model = new()
         {
             Amenity = villaDTO.Amenity,
             Details = villaDTO.Details,
-            Id = villaDTO.Id,
             ImageUrl = villaDTO.ImageUrl,
             Name = villaDTO.Name,
             Occupancy = villaDTO.Occupancy,
@@ -92,7 +92,7 @@ public class VillaAPIController : ControllerBase
         };
         _db.Villas.Add(model);
         _db.SaveChanges();
-        return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+        return CreatedAtRoute("GetVilla", new { id = model.Id }, model);
     }
 
     #endregion
@@ -128,7 +128,7 @@ public class VillaAPIController : ControllerBase
     [HttpPut("{id:int}", Name = "UpdateVilla")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+    public IActionResult UpdateVilla(int id, [FromBody] VillaUpdateDTO villaDTO)
     {
         if (villaDTO == null || id != villaDTO.Id)
         {
@@ -158,16 +158,16 @@ public class VillaAPIController : ControllerBase
     [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaUpdateDTO> patchDTO)
     {
         if (patchDTO == null || id == 0)
         {
             return BadRequest();
         }
 
-        var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+        var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
 
-        VillaDTO villaDTO = new()
+        VillaUpdateDTO villaDTO = new()
         {
             Amenity = villa.Amenity,
             Details = villa.Details,
@@ -209,4 +209,4 @@ public class VillaAPIController : ControllerBase
     }
 
     #endregion
-} 
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -4,17 +4,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddDbContext<ApplicationDbContext>(option =>
-{
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
-builder.Services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; })
-    .AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+builder.Services.AddControllers(option => { 
+    //option.ReturnHttpNotAcceptable=true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,7 +25,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
- 
+
 app.MapControllers();
 
 app.Run();
