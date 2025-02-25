d29f8caba0f42123f62f8eca8a3af6091684d8a4
kourosh karimi <karimikorosh2000@gmail.com>
2025-02-25 19:07:53 +0330

    Include Villa when retireving Villa Number

--- MagicVilla_VillaAPI/Repository/Repo.cs
+++ MagicVilla_VillaAPI/Repository/Repo.cs
@@ -15,6 +15,7 @@ public class Repo<T> : IRepo<T> where T : class
     public Repo(ApplicationDbContext db)
     {
         _db = db;
+        // _db.VillaNumbers.Include(u => u.Villa).ToList();
         this.dbSet = _db.Set<T>();
     }
 
@@ -26,7 +27,8 @@ public class Repo<T> : IRepo<T> where T : class
         await SaveAsync();
     }
 
-    public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+    public async Task<T> GetAsync
+        (Expression<Func<T, bool>> filter = null, bool tracked = true, string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
         if (!tracked)
@@ -39,16 +41,35 @@ public class Repo<T> : IRepo<T> where T : class
             query = query.Where(filter);
         }
 
+        if (includeProperties != null)
+        {
+            foreach (var includeProp in includeProperties.Split(new char[] { ',' },
+                         StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProp);
+            }
+        }
+
         return await query.FirstOrDefaultAsync();
     }
 
-    public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+    public async Task<List<T>> GetAllAsync
+        (Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
 
         if (filter != null)
         {
             query = query.Where(filter);
+        }
+
+        if (includeProperties != null)
+        {
+            foreach (var includeProp in includeProperties.Split(new char[] { ',' },
+                         StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProp);
+            }
         }
 
         return await query.ToListAsync();
